797f181dd9d3983febd6efb896b9d7b35743eaa1
Gustavo Cortelassi <gutocortelassi72@gmail.com>
2024-03-06 18:26:26 -0300

    menu de seleção das operações

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Calculadora {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int op;
         
+        do{
+            System.out.println("--- CALCULADORA ---");
+            System.out.println("Informe a operação que deseja realizar: ");
+            System.out.println("1. Soma ");
+            System.out.println("2. Subtracao");
+            System.out.println("3. Divisao");
+            System.out.println("4. Multiplicacao");
+            System.out.println("0. Sair");
+            op = scanner.nextInt();
+
+        } while (op != 0);
     }
+
+    public int Soma(int a, int b){
+        int soma = a + b;
+        return soma;
+    };
 }
